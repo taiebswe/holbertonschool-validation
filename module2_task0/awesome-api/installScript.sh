e365246aa7458355cbe2496b61983a762cd76a5c
Taieb CH <taieb.chaabini@holbertonschool.com>
2023-01-17 10:26:29 +0000

    Update

--- module2_task0/awesome-api/installScript.sh
+++
@@ -1,2 +0,0 @@
-wget https://go.dev/dl/go1.19.5.linux-amd64.tar.gz && tar -xf go1.19.5.linux-amd64.tar.gz && rm go1.19.5.linux-amd64.tar.gz && cp go/bin/go /usr/bin/go && mv go /usr/local/go
-go get github.com/gorilla/mux
